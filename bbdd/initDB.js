f36161613d923e0309e7537038fa882c0feeb8a8
¨braisgf¨ <braisgf@gmail.com>
2023-01-02 11:51:03 +0100

    Add error middlewares

---
+++ bbdd/initDB.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const mysql = require("mysql2/promise");
+const getConnection = require("./getConnection");
+const bcrypt = require("bcrypt");
+
+const main = async () => {
+  let connection;
+  try {
+    connection = await getConnection();
+
+    console.log("Cleaning tables...");
+
+    await connection.query(`DROP TABLE IF EXISTS users `);
+    await connection.query(`DROP TABLE IF EXISTS files `);
+
+    console.log("Clean succesfully!");
+    console.log("Creating tables...");
+
+    await connection.query(`
+    CREATE TABLE users (
+        id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        photo VARCHAR(255),
+        createdAt TIMESTAMP NOT NULL
+      )
+    `);
+
+    await connection.query(`
+      CREATE TABLE files (
+        id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+        user_id INT UNSIGNED NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users(id),
+        name VARCHAR(255) NOT NULL,
+        createdAt TIMESTAMP NOT NULL
+      );
+      `);
+
+    console.log("Create Succesfully!");
+
+    //CREACION DE UN ADMIN
+
+    //Encriptar la contrasena
+    const adminPass = await bcrypt.hash(process.env.ADMIN_PASS, 10);
+
+    //Insertar el admin en la BBDD
+    await connection.query(
+      `
+    INSERT INTO  users (username, email, password, createdAt)
+    VALUES ('admin', ?, ?, ?)
+    `,
+      [process.env.ADMIN_EMAIL, adminPass, new Date()]
+    );
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+main();
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+//MW Morgan
+app.use(morgan("dev"));
+
+//MW ERROR
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send({
+    status: "Error",
+    message: err.message,
+  });
+});
+//MW 404
+app.use((req, res) => {
+  res.status(404);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening at ${process.env.PORT}`);
