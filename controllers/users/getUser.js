c98622b8daf7f31bd1ae4fb2bec8baad666a5477
¨braisgf¨ <braisgf@gmail.com>
2023-02-13 20:01:38 +0100

    Modified getUser

--- controllers/users/getUser.js
+++ controllers/users/getUser.js
@@ -11,6 +11,7 @@ const getUser = async (req, res, next) => {
 
     // Objeto con información básica del usuario.
     const userInfo = {
+      id: user.id,
       username: user.username,
       photo: user.photo,
       bio: user.bio,
